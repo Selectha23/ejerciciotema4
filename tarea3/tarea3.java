438e6b645ed423efd0bdb0a1a4bc37745c6a0546
Selectha23 <seleriddim507@gmail.com>
2023-03-28 15:59:02 -0500

    bucle do while

---
+++ tarea3/tarea3.java
@@ -0,0 +1,13 @@
+package tarea3;
+
+public class tarea3 {
+    public static void main(String[] args) {
+        int pa = 0;
+
+        do{
+            System.out.println(pa);
+            pa++;
+
+        }while(pa < 3);
+    }
+}
