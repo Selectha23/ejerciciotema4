e9f7ebb0b165336784f1dfabf7133b543d25e1fd
Selectha23 <seleriddim507@gmail.com>
2023-03-28 14:17:20 -0500

    ejercicio de practica sobre if, whaile

---
+++ posi/posi.java
@@ -0,0 +1,25 @@
+package posi;
+
+/*  Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+Pista: Los números inferiores a 0 son negativos y los superiores, positivos.*/
+public class posi {
+    
+public static void main(String[] args) {
+
+    int numeroIf = 0;
+
+    if(numeroIf < 0){
+        System.out.println("es negativo");
+
+    }else if(numeroIf > 0){
+        System.out.println("es positivo");
+    }else if (numeroIf == 0){
+        System.out.println("es cero");
+    }
+
+
+    
+}
+
+
+}
